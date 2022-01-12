0a11defc24019c6ad9ebebc34feb5188d334551a
Ian Graham <irgraham1@gmail.com>
2022-01-12 08:22:32 -0500

    more edits to md example

--- md-example/src/lib.rs
+++ md-example/src/lib.rs
@@ -34,6 +34,8 @@ impl Simulation {
             self.apply_buffer_to_forces();
 
             self.integrate_dynamics();
+
+            self.timestep += 1;
         }
     }
 
@@ -53,7 +55,7 @@ impl Simulation {
     fn update_force_buffer(&mut self) {
         assert_ne!(self.potentials.len(), 0);
         for pot in self.potentials.iter() {
-            pot.update_to_force_buffer(&mut self.configuration, &mut self.workhorse);
+            pot.update_to_force_buffer(&self.configuration, &mut self.workhorse);
         }
     }
 
@@ -86,6 +88,37 @@ struct Configuration {
     types: Vec<u8>
 }
 
+impl Configuration {
+    
+    fn pbc_vec(&self, i: u32, j: u32) -> [f32; 3] {
+
+        let mut vec = [0.0f32; 3];
+
+        { // scope to do mut Zip on NdProducer of underlying slice
+            let mut vec = ArrayViewMut1::from(&mut vec[..self.dim as usize]);
+            Zip::from(&self.position.row(i as usize))
+                .and(&self.position.row(j as usize))
+                .and(&mut vec)
+                .for_each(
+                    |xi, xj, v| *v = xi - xj
+                );
+            
+            // apply PBCs
+            todo!();
+        }
+
+        vec
+    }
+
+    fn inv_norm_squared(&self, vec: &[f32; 3]) -> f32 {
+        let x = vec.iter()
+            .take(self.dim as usize)
+            .fold(0.0, |acc, &x| acc + x*x);
+        1.0/x
+    }
+
+}
+
 #[derive(Default)]
 struct SimWorkhorse {
     nlist_force_buffer: Vec<[f32;3]>,
@@ -113,22 +146,52 @@ trait Integrator {
 
 // takes information of simulation state and computes forces
 trait Potential {
-    fn update_to_force_buffer(&self, config: &mut Configuration, workhorse: &mut SimWorkhorse) {
+    fn update_to_force_buffer(&self, config: &Configuration, workhorse: &mut SimWorkhorse) {
+        for ((i, j), f) in workhorse.nlist_query.iter().zip(&mut workhorse.nlist_force_buffer) {
+            let dv = config.pbc_vec(*i, *j);
+            let invr2 = config.inv_norm_squared(&dv);
 
+            let force = self.force(invr2, config.type_ids[*i as usize], config.type_ids[*i as usize]);
+        }
     }
 
-    fn force(&self, dr: f32, type_i: u8, type_j: u8) -> f32;
+    fn force(&self, invr2: f32, type_i: u8, type_j: u8) -> f32;
+
+    fn max_cutoff(&self) -> f32;
 }
 
-struct LJPotential {
-    lj1: f32,
-    lj2: f32
+struct BiLjPotential {
+    lj1: [f32; 3],
+    lj2: [f32; 3],
+    cut: [f32; 3]
 }
 
-impl Potential for LJPotential {
+impl Default for BiLjPotential {
+    fn default() -> Self {
+        let aa: (f32, f32) = (1.0, 1.0);
+        let ab: (f32, f32) = (1.5, 0.8);
+        let bb: (f32, f32) = (0.5, 0.88);
 
-    fn force(&self, dr: f32, type_i: u8, type_j: u8) -> f32 {
-        0.0
+        let lj1 = |(epsilon, sigma): (f32, f32)| 48.0*epsilon*sigma.powi(12);
+        let lj2 = |(epsilon, sigma): (f32, f32)| -24.0*epsilon*sigma.powi(6);
+        let cut = |(_, sigma): (f32, f32)| 2.5*sigma;
+
+        BiLjPotential{
+            lj1: [lj1(aa), lj1(ab), lj1(bb)],
+            lj2: [lj2(aa), lj2(ab), lj2(bb)],
+            cut: [cut(aa), cut(ab), cut(bb)]
+        }
+    }
+}
+
+impl Potential for BiLjPotential {
+
+    fn force(&self, invr2: f32, type_i: u8, type_j: u8) -> f32 {
+        todo!();
+    }
+
+    fn max_cutoff(&self) -> f32 {
+        self.cut.into_iter().reduce(f32::max).unwrap()
     }
 }
 
@@ -136,7 +199,7 @@ type Pair = (u32, u32);
 type NeighborListQuery = [Pair];
 
 // checks that particles are still in their boxes, updates box if necessary
-trait NeighborList: Component {
+trait NeighborList {
     fn query(&mut self, sim: &Configuration) -> &NeighborListQuery;
 }
 
@@ -147,7 +210,7 @@ struct CellList {
     stride: Box<[f64]>
 }
 
-impl Component for CellList { }
+// impl Component for CellList { }
 
 impl NeighborList for CellList {
     
@@ -157,5 +220,5 @@ impl NeighborList for CellList {
     }
 }
 
-trait Component {
-}
+// trait Component {
+// }
