6676dbc73f582b8c6ab12e27938b5df5e44068d4
Ian Graham <irgraham1@gmail.com>
2022-02-13 16:58:42 -0500

    removed guessing_game example

---
+++ part-1/ownership/src/main.rs
@@ -0,0 +1,7 @@
+// Now that we have a grasp of Rust syntax, let's get into a bit of the meat of 
+// the language. One of the critical concepts to grasp in Rust is that of
+// ownership.
+
+fn main() {
+    
+}
